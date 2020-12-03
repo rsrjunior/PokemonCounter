288dcde6b3dfa095bdfec1e76b776e4b903fcee5
Roberto Rosa Junior <beto.junior@gmail.com>
2020-12-03 05:34:17 -0300

    Refactoring Regex string

--- PokemonCounterApp/PokemonCounter.cs
+++ PokemonCounterApp/PokemonCounter.cs
@@ -4,50 +4,57 @@ using System.Text.RegularExpressions;
 namespace PokemonCounterApp
 {
     public class PokemonCounter
-{
-    public int Count { get; private set;}
-    private HashSet<string> pokeBall;
-
-    private AshCoordinates currentPos;
-    public PokemonCounter()
     {
-        Count = 1;
-        currentPos = new AshCoordinates(0,0);
-        pokeBall = new HashSet<string>();
-        pokeBall.Add(currentPos.ToString());
-    }
-    public int ProcessChar(char direction)
-    {
-        int pokemon = 0;
-        if ("EONS".Contains(direction))
+        public int Count { get; private set; }
+        private HashSet<string> pokeBall;
+        private List<string> oneDirectionRegex;
+        private AshCoordinates currentPos;
+        public PokemonCounter()
         {
-            currentPos.Move(direction);
-            if(pokeBall.Add(currentPos.ToString()))
+            Count = 1;
+            currentPos = new AshCoordinates(0, 0);
+            pokeBall = new HashSet<string>();
+            pokeBall.Add(currentPos.ToString());
+            oneDirectionRegex = new List<string>(5);
+            oneDirectionRegex.Add("^E+$|^O+$|^N+$|^S+$");
+        }
+        public int ProcessChar(char direction)
+        {
+            int pokemon = 0;
+            if ("EONS".Contains(direction))
             {
-                pokemon = 1;
-                Count++;
+                currentPos.Move(direction);
+                if (pokeBall.Add(currentPos.ToString()))
+                {
+                    pokemon = 1;
+                    Count++;
+                }
             }
+            return pokemon;
         }
-        return pokemon;
-    }
-    public int ProcessLine(string movements)
-    {
-        int pokemons = 0;
-
-        Match m = Regex.Match(movements,"^E+$|^O+$|^N+$|^S+$");
-        if(m.Success)
+        public int ProcessLine(string movements)
         {
-            Count+=movements.Length;
-            pokemons+=movements.Length;
-            movements = "";
+            int pokemons = 0;
+
+            Match m;
+            foreach (string item in oneDirectionRegex)
+            {
+                m = Regex.Match(movements, item);
+                if (m.Success)
+                {
+                    Count += movements.Length;
+                    pokemons += movements.Length;
+                    movements = "";
+                }
+            }
+
+
+            foreach (char direction in movements)
+            {
+                pokemons += ProcessChar(direction);
+            }
+            return pokemons;
         }
 
-        foreach (char direction in movements)
-        {
-            pokemons += ProcessChar(direction); 
-        }
-        return pokemons;
     }
-    
-}
 }
